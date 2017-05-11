31313105d8d6dcba54513b4dea84d469e5778b83
Dion MacIntyre <dion@macintyres.me.uk>
2017-05-11 20:27:56 +0100

    Full functionality

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,39 @@ authenticate();
 $('.logout').click(function(){
 	alert('Logged out');
 	window.localStorage.removeItem("token");
+	window.location.replace("/index.html");
+});
+
+$('.orderList').live('click', '.orderListItem', function(event){
+	//Gets the id associated with the clicked order. Due to the ajax
+	//loading of the orders .click() can't be used on the specific
+	//.orderListItem so .live() on the parent element is used. Also,
+	//due to the h3 element it is possible the click can be tracked to
+	//either the li or h3 elements, this is accounted for below
+	
+	var target = event.target;
+	var id = '';
+	if(target.localName === 'h3'){
+		id = target.parentElement.attributes['data-id'].value;
+	}else{
+		id = target.attributes['data-id'].value;
+	}
+
+
+	orderHub.server.orderDetails(id);
+	$('.orderContent').css('visibility', 'visible');
+});
+
+$('#orderComplete').click(function(){
+	if(confirm('Is this order complete?')){
+		orderHub.server.orderComplete($(this).attr('data-id'));
+	}
+});
+
+$('#orderCollected').click(function(){
+	if(confirm('Has the order been collected?')){
+		orderHub.server.orderCollected($(this).attr('data-id'));
+	}
 });
 
 //Functions
@@ -39,7 +72,7 @@ function login(username, password){
 	}).done(function(result){
 		console.log(result);
 		if(result.role === "Admin"){
-			window.localStorage.token = "Bearer " + result.access_token;
+			window.localStorage.token = result.access_token;
 			window.location.replace("/order.html");
 		} else{
 			alert("You don't have permission to access this system");
@@ -66,17 +99,79 @@ function authenticate(){
 	}
 }
 
+function disableButton(){
+	$('#orderCollected').removeAttr('disabled');
+	$('#orderComplete').attr('disabled', 'true');
+}
+
+function enableButton(){
+	$('#orderComplete').removeAttr('disabled');
+	$('#orderCollected').attr('disabled', 'true');
+}
 //SignalR
-$.connection.hub.url = 'https://api.furysayr.co.uk/signalr'
-var order = $.connection.orderHub;
+$.connection.hub.url = url + '/signalr'
+var orderHub = $.connection.orderHub;
 var token = window.localStorage.token;
 
-$.connection.hub.qs = { "bearerToken" : token}
+$.connection.hub.qs = { "bearerToken" : token }
 
-order.client.test = function(data){
+orderHub.client.displayOrders = function(data){
+	for(order of data){
+		$('#displayOrders').append('<li class="orderListItem" data-id="'+ order.Id + '"><h3>Order ' + order.OrderNumber + '</h3></li>');
+	}
+}
+orderHub.client.newOrder = function(order){
+	$('#displayOrders').append('<li class="orderListItem" data-id="'+ order.Id + '"><h3>Order ' + order.OrderNumber + '</h3></li>');
+}
+orderHub.client.orderDetails = function(data){
+	var drinkList = {}
+	$('.drinkSidebar').html('');
+	enableButton();
+
+	$('.orderContentImage').html('<img src="https://www.placecage.com/150/150">');
+	$('.orderContentMain h2').html('Order ' + data.OrderNumber);
+	$('.orderContentWOD h1').text(data.OrderWord);
+	$('#orderComplete').attr('data-id', data.OrderId);
+	$('#orderCollected').attr('data-id', data.OrderId);
+	if(data.Completed == true){
+		disabledButton();
+	}
+
+	for(drink of data.Drinks){
+		if(drinkList[drink.Id]){
+			drinkList[drink.Id]++;
+		} else{
+			drinkList[drink.Id] = 1;
+			$('.drinkSidebar').append('<li class="orderListItem" data-id="' + drink.Id + '"><h3 class="quantity"></h3><h3>' + drink.Name + '</h3></li>');
+		}
+		$('.orderListItem[data-id="' + drink.Id + '"] h3').first().text(drinkList[drink.Id]);
+	}
+
+}
+
+orderHub.client.orderComplete = function(data){
+	console.log(data);
+	if(data.success){
+		disableButton();
+	}
+};
+
+orderHub.client.orderCollected = function(data){
+	console.log(data);
+	if(data.success){
+		$('#orderCollected').attr('disabled', 'true');
+		$('.orderContent').css('visibility', 'hidden');
+		$('.orderListItem[data-id="' + data.orderId + '"]').remove();
+	}
+};
+
+orderHub.client.error = function(data){
+	console.error(data);
+}
+orderHub.client.userConnected = function(data){
 	console.info(data);
 }
 
 $.connection.hub.start().done(function(){
-	console.log('Started');
+
 });
